a2a54fa6c98ce095be76dff0bf27ad1819b3d831
JC <jorge.e.colindres@gmail.com>
2014-03-02 14:01:53 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# jQuery.Trellis
+
+Lightweight masonry layouts.
+
+## Getting Started
+
+Download the [production version][min] or the [development version][max].
+
+[min]: https://raw.github.com/corporadobob/jquery-trellis/master/dist/jquery.trellis.min.js
+[max]: https://raw.github.com/corporadobob/jquery-trellis/master/dist/jquery.trellis.js
+
+In your web page:
+
+```html
+<script src="jquery.js"></script>
+<script src="dist/trellis.min.js"></script>
+<script>
+jQuery(function($) {
+  $.awesome(); // "awesome"
+});
+</script>
+```
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Release History
+_(Nothing yet)_
---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+'use strict';
+
+var gulp = require('gulp');
+
+// Load plugins
+var _ = require('gulp-load-plugins')();
+
+// Javascripts
+gulp.task('scripts', function () {
+  return gulp.src('src/**/*.js')
+  .pipe(_.jshint('.jshintrc'))
+  .pipe(_.jshint.reporter('jshint-stylish'))
+  .pipe(_.jshint.reporter('fail'))
+  .pipe(_.size({ showFiles: true }))
+  .pipe(gulp.dest('dist'))
+  .pipe(_.uglify())
+  .pipe(_.rename({ suffix: '.min' }))
+  .pipe(_.size({ showFiles: true }))
+  .pipe(gulp.dest('dist'));
+});
+
+// Clean
+gulp.task('clean', function () {
+  return gulp.src(['dist/scripts'], { read: false }).pipe(_.clean());
+});
+
+// Default task
+gulp.task('default', ['clean'], function () {
+  gulp.start('scripts');
+});
+
+// Watch
+gulp.task('watch', ['scripts'], function () {
+
+  // Watch .js files
+  gulp.watch('src/**/*.js', ['scripts']);
+});
